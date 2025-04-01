8749320742ad60391bbf01e8fef63f7635e63b47
Anne S. Warlaumont <warlaumont@ucla.edu>
2025-04-01 15:42:54 -0700

    Update reservoirnet_tutorial.R
    Finished working through tutorial

--- reservoirnet_tutorial.R
+++ reservoirnet_tutorial.R
@@ -45,3 +45,63 @@ X <- dfOutcome %>%
 reservoir_state <- predict_seq(node = reservoir, X = X, reset = TRUE)
 
 plot(reservoir_state)
+
+stand_max <- function(x) return(x/max(x))
+
+# scaled features
+Xstand <- dfOutcome %>%
+  filter(date < traintest_date) %>%
+  select(hosp, Positive, Tested) %>%
+  mutate_all(.funs = stand_max) %>%
+  as.matrix() %>%
+  as.array()
+# feed them to the reservoir
+reservoir_state_stand <- predict_seq(node = reservoir, X = Xstand, reset = TRUE)
+# plot the output
+plot(reservoir_state_stand)
+
+readout <- reservoirnet::createNode(nodeType = "Ridge", ridge = 0.1)
+model <- reservoirnet::link(reservoir, readout)
+
+# train set
+yTrain <- dfOutcome %>% filter(outcomeDate <= traintest_date) %>% select(outcome)
+xTrain <- dfOutcome %>% filter(outcomeDate <= traintest_date) %>% select(hosp, Positive, Tested)
+# test set
+xTest <- dfOutcome %>% select(hosp, Positive, Tested)
+
+# standardize based on training set values
+ls_fct_stand <- apply(xTrain,
+                      MARGIN = 2,
+                      FUN = function(x) function(feature) return(feature/(max(x))))
+
+xTrainstand <- xTrain
+xTeststand <- xTest
+lapply(X = names(ls_fct_stand),
+       FUN = function(x){
+         xTrainstand[,x] <<- ls_fct_stand[[x]](feature = xTrain[,x])
+         xTeststand[,x] <<- ls_fct_stand[[x]](feature = xTest[,x])
+         return()
+       })
+
+# convert to array
+lsdf <- lapply(list(yTrain = yTrain,
+                    xTrain = xTrainstand,
+                    xTest = xTeststand),
+               function(x) as.array(as.matrix(x)))
+
+### train the reservoir ridge output
+fit <- reservoirnet::reservoirR_fit(node = model, X = lsdf$xTrain, Y = lsdf$yTrain, warmup = 30, reset = TRUE)
+
+### predict with the reservoir
+vec_pred <- reservoirnet::predict_seq(node = fit$fit, X = lsdf$xTest, reset = TRUE)
+
+dfOutcome %>%
+  mutate(pred = vec_pred) %>%
+  ggplot(mapping = aes(x = outcomeDate)) +
+  geom_line(mapping = aes(y = outcome, color = "observed")) +
+  geom_line(mapping = aes(y = pred, color = "forecast")) +
+  geom_vline(mapping = aes(color = "train-test sets", xintercept = traintest_date)) +
+  scale_color_manual(values = c("#3772ff", "#080708", "#df2935")) +
+  theme_bw() +
+  labs(color = "", x = "Date", y = "Hospitalisations")
+       })
