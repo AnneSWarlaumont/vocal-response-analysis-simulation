aef4fa7f3c32da7c7d6fabf57b83525a130bb7df
Anne S. Warlaumont <warlaumont@ucla.edu>
2025-04-22 15:16:56 -0700

    Update VocEventSim_Optimize.R
    Added code to count number of conversational turns

--- VocEventSim_Optimize.R
+++ VocEventSim_Optimize.R
@@ -10,9 +10,9 @@ lena_segments = read.csv("0344_000913_segments.csv")
 chn_segments = subset(lena_segments,segtype=="CHNSP"|segtype=="CHNNSP")
 adu_segments = subset(lena_segments,segtype=="FAN"|segtype=="MAN")
 rec_length = floor(lena_segments$endsec[nrow(lena_segments)])
-chn_voc_record = numeric(rec_length)
+chn_voc_record = integer(rec_length)
 chn_voc_record[as.integer(floor(chn_segments$startsec))] = 1
-adu_voc_record = numeric(rec_length)
+adu_voc_record = integer(rec_length)
 adu_voc_record[as.integer(floor(adu_segments$startsec))] = 1
 
 ####################################
@@ -24,8 +24,33 @@ chn_total = sum(chn_voc_record)
 adu_total = sum(adu_voc_record)
 
 # Number of conversational turns based on a 5 s threshold
-# Initialize leader and follower ID and time
-# Loop through each second of simulation
+# Initialize leader ID and time
+# Loop through each second of the recording
 # When a turn is identified, update the turn count
+turnCount = 0
+leadID = 'none'
+leadT = 0
+for (t in 1:rec_length){
+  leadT = leadT+1
+  if (leadID=='chn' && adu_voc_record[t]==1) {
+    if (leadT <= 5) {
+      turnCount = turnCount+1
+    }
+    leadID = 'adu'
+    leadT = 0
+  } else if (leadID=='adu' && chn_voc_record[t]==1) {
+    if (leadT <= 5) {
+      turnCount = turnCount+1
+    }
+    leadID = 'chn'
+    leadT = 0
+  } else if (leadID=='none' && adu_voc_record[t]==1) {
+    leadID = 'adu'
+    leadT = 0
+  } else if (leadID=='none' && chn_voc_record[t]==1) {
+    leadID = 'chn'
+    leadT = 0
+  }
+}
 
 # 10th, 50th, and 90th percentiles of the child and adult IEI distributions
