07c205e56e68f895b07337d84cc4dc34390845b1
Anne S. Warlaumont <warlaumont@ucla.edu>
2025-04-23 15:32:57 -0700

    Update VocEventSim_Optimize.R
    Added code to get IVI percentile measures.

--- VocEventSim_Optimize.R
+++ VocEventSim_Optimize.R
@@ -53,4 +53,29 @@ for (t in 1:rec_length){
   }
 }
 
-# 10th, 50th, and 90th percentiles of the child and adult IEI distributions
+# 10th, 50th, and 90th percentiles of the child and adult ivi distributions
+# try the quantile function
+get_ivis <- function(voc_record){
+  ivi_record = c()
+  voc1_t = NA # initialize the time of the first vocalization in the current IVI pair
+  voc2_t = NA # initialize the time of the second vocalization in the current IVI pair 
+  for (t in 1:length(voc_record)){
+    if (is.na(voc1_t) & voc_record[t]==1){
+      voc1_t = t
+    } else if (voc_record[t]==1){
+      voc2_t = t
+      ivi = voc2_t - voc1_t
+      ivi_record = c(ivi_record,ivi)
+      voc1_t = voc2_t
+    }
+  }
+  return(ivi_record)
+}
+chn_ivi_record = get_ivis(chn_voc_record)
+adu_ivi_record = get_ivis(adu_voc_record)
+chn_ivi_10 = quantile(chn_ivi_record,probs=.1,names=FALSE)
+chn_ivi_50 = quantile(chn_ivi_record,probs=.5,names=FALSE)
+chn_ivi_90 = quantile(chn_ivi_record,probs=.9,names=FALSE)
+adu_ivi_10 = quantile(adu_ivi_record,probs=.1,names=FALSE)
+adu_ivi_50 = quantile(adu_ivi_record,probs=.5,names=FALSE)
+adu_ivi_90 = quantile(adu_ivi_record,probs=.9,names=FALSE)
