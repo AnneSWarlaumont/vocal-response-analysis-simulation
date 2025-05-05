89c205e54be84ff6f5e57be116d6695150d1a1c6
Anne S. Warlaumont <warlaumont@ucla.edu>
2025-05-05 14:51:01 -0700

    analysis updates

--- VocEventSim_Optimize.R
+++ VocEventSim_Optimize.R
@@ -310,7 +310,11 @@ sim_turnCount_scaled = scale(log(sims_df$sim_turnCount+1))
 sims_df$sim_turnCount_scaled = sim_turnCount_scaled[,1]
 turnCount_scaled = (log(turnCount+1)-attr(sim_turnCount_scaled,"scaled:center"))/attr(sim_turnCount_scaled,"scaled:scale")
 
-sims_df$simDist = sqrt((sims_df$chn_sim_total_scaled-chn_total_scaled)^2+(sims_df$chn_sim_n_ivi_1_scaled-chn_n_ivi_1_scaled)^2+(sims_df$chn_sim_ivi_50_scaled-chn_ivi_50_scaled)^2+(sims_df$chn_sim_ivi_max_scaled-chn_ivi_max_scaled)^2+(sims_df$adu_sim_total_scaled-adu_total_scaled)^2+(sims_df$adu_sim_n_ivi_1_scaled-adu_n_ivi_1_scaled)^2+(sims_df$adu_sim_ivi_50_scaled-adu_ivi_50_scaled)^2+(sims_df$adu_sim_ivi_max_scaled-adu_ivi_max_scaled)^2+(sims_df$sim_turnCount_scaled-turnCount_scaled)^2)
+if (nonInteractiveSim == TRUE){
+  sims_df$simDist = sqrt((sims_df$chn_sim_total_scaled-chn_total_scaled)^2+(sims_df$chn_sim_n_ivi_1_scaled-chn_n_ivi_1_scaled)^2+(sims_df$chn_sim_ivi_50_scaled-chn_ivi_50_scaled)^2+(sims_df$chn_sim_ivi_max_scaled-chn_ivi_max_scaled)^2+(sims_df$adu_sim_total_scaled-adu_total_scaled)^2+(sims_df$adu_sim_n_ivi_1_scaled-adu_n_ivi_1_scaled)^2+(sims_df$adu_sim_ivi_50_scaled-adu_ivi_50_scaled)^2+(sims_df$adu_sim_ivi_max_scaled-adu_ivi_max_scaled)^2)
+} else{
+  sims_df$simDist = sqrt((sims_df$chn_sim_total_scaled-chn_total_scaled)^2+(sims_df$chn_sim_n_ivi_1_scaled-chn_n_ivi_1_scaled)^2+(sims_df$chn_sim_ivi_50_scaled-chn_ivi_50_scaled)^2+(sims_df$chn_sim_ivi_max_scaled-chn_ivi_max_scaled)^2+(sims_df$adu_sim_total_scaled-adu_total_scaled)^2+(sims_df$adu_sim_n_ivi_1_scaled-adu_n_ivi_1_scaled)^2+(sims_df$adu_sim_ivi_50_scaled-adu_ivi_50_scaled)^2+(sims_df$adu_sim_ivi_max_scaled-adu_ivi_max_scaled)^2+(sims_df$sim_turnCount_scaled-turnCount_scaled)^2)
+}
 
 fitOrder = order(sims_df$simDist)
 
@@ -384,5 +388,71 @@ save.image("VocEventSim_Optimize_20250505.RData")
 save(sims_adu_ivi_records,sims_chn_ivi_records,sims_df,fitOrder,file = "VocEventSim_Optimize_20250505_subset1.RData")
 save(sims_adu_voc_records,sims_chn_voc_records,file = "VocEventSim_Optimize_20250505_subset.RData")
 
-# for the best-matched simulation, analyze the chn and adu ivis using our two response analysis procedures
+#################################################################################
+# for the best-matched simulation,
+# analyze the chn and adu ivis to test for response effects on ivis
+# using 1. no control for previous ivi and 2. control for previous ivi or 2 ivis
+#################################################################################
 
+rthresh = 5
+
+simID = 0
+ivi_records = c()
+ivi_r_records = c()
+simIDs=c()
+previvi_resids = c()
+prev3ivi_resids = c()
+
+for (fitRank in 1:20){
+  
+  simID = simID + 1
+  
+  ivi_series = sims_chn_ivi_records[[fitOrder[fitRank]]]
+  chn_voc_record = sims_chn_voc_records[[fitOrder[fitRank]]]
+  adu_voc_record = sims_adu_voc_records[[fitOrder[fitRank]]]
+  
+  n_ivi = length(ivi_series)
+  
+  
+  ivi_r_record = c() # initialize the record of when adu responded to chn
+  t = 1
+  for (i in 1:n_ivi){
+    if (ivi_series[i]==1){
+      ivi_r_record[i] = NA
+    } else if (sum(adu_voc_record[(t+1):(t+rthresh)])>0){
+      ivi_r_record[i] = 1
+    } else{
+      ivi_r_record[i] = 0
+    }
+    t = t+ivi_series[i]
+  }
+  
+  ivi_records = c(ivi_records,ivi_series[4:n_ivi])
+  ivi_r_records = c(ivi_r_records,ivi_r_record[4:n_ivi])
+  simIDs = c(simIDs,rep(simID,(n_ivi-3)))
+  
+  # Correlate current IVI with previous IVI and get the residuals of that correlation
+  previvi_model = lm(scale(log(ivi_series[2:n_ivi]))~scale(log(ivi_series[1:(n_ivi-1)])))
+  previvi_resid = resid(previvi_model)
+  previvi_resids = c(previvi_resids,previvi_resid[3:(n_ivi-1)])
+  
+  # Correlate current IVI with time since the past 3 a1 IVIs and get the residuals of that correlation
+  prev3ivi_model = lm(scale(log(ivi_series[4:n_ivi]))~scale(log(ivi_series[3:(n_ivi-1)]))+(scale(log(ivi_series[3:(n_ivi-1)])+log(ivi_series[2:(n_ivi-2)])))+(scale(log(ivi_series[3:(n_ivi-1)])+log(ivi_series[2:(n_ivi-2)])+log(ivi_series[1:(n_ivi-3)]))))
+  prev3ivi_resid = resid(prev3ivi_model)
+  prev3ivi_resids = c(prev3ivi_resids,prev3ivi_resid)
+  
+}
+
+# Analyze using IVI-based approaches (our original and its variants controlling for previous IVIs)
+ivi_models = analyze_ivis(ivi_records,ivi_r_records,previvi_resids,prev3ivi_resids,simIDs)
+uncontrolled_response_model = ivi_models[[1]]
+residual_response_model = ivi_models[[2]]
+prev3residual_response_model = ivi_models[[3]]
+
+summary(uncontrolled_response_model)
+summary(residual_response_model)
+summary(prev3residual_response_model)
+
+#################################################################################
+# Plot fit to human data as a function of parameter value
+#################################################################################
