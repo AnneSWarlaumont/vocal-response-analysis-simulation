37a96e19c274a82a7de691bba8d306b5d5065f84
Anne S. Warlaumont <warlaumont@ucla.edu>
2025-05-19 11:59:54 -0700

    Update MergeSimData.R

--- MergeSimData.R
+++ MergeSimData.R
@@ -3,6 +3,11 @@
 # optimization is pure random search. So we can merge the three sets of
 # simulation runs and then re-do the search for best fits.
 
-recordings = c("0344_000913","0833_010606","0054_000603")
 simTypes = c("nonInteractive","a2interactive","bidirectional")
+recordings = c("0344_000913","0833_010606","0054_000603")
 
+for (simType in simTypes){
+  for (recording in recordings){
+    workspace_fileAndPath = Sys.glob(paste("~/Documents/GitHub/vocal-response-analysis-simulation/data/",recording,"/",simType,"/*",sep=""))
+    load(workspace_fileAndPath)
+}
