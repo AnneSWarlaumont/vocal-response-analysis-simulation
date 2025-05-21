20a2eebea0f2cd76b35298056d5b732e844536e3
Anne S. Warlaumont <warlaumont@ucla.edu>
2025-05-21 11:06:00 -0700

    Worked on batch merge and tidying

--- MergeSimData.R
+++ MergeSimData.R
@@ -6,16 +6,32 @@
 simTypes2merge = c("nonInteractive","a2interactive","bidirectional")
 recordings2merge = c("0344_000913","0833_010606","0054_000603")
 
+# # For development and debugging:
+# simTypes2merge = c("nonInteractive")
+# recordings2merge = c("0344_000913")
+
 for (simType2merge in simTypes2merge){
   for (recording2merge in recordings2merge){
     workspace_fileAndPath = Sys.glob(paste("~/Documents/GitHub/vocal-response-analysis-simulation/data/",recording2merge,"/",simType2merge,"/*",sep=""))
     load(workspace_fileAndPath)
     if (exists("sims_df_merged")){
       sims_df_merged = rbind(sims_df_merged,sims_df)
+      sims_adu_ivi_records_merged = c(sims_adu_ivi_records_merged,sims_adu_ivi_records)
+      sims_adu_voc_records_merged = c(sims_adu_voc_records_merged,sims_adu_voc_records)
+      sims_chn_ivi_records_merged = c(sims_chn_ivi_records_merged,sims_chn_ivi_records)
+      sims_chn_voc_records_merged = c(sims_chn_voc_records_merged,sims_chn_voc_records)
     } else{
       sims_df_merged = sims_df
+      sims_adu_ivi_records_merged = sims_adu_ivi_records
+      sims_adu_voc_records_merged = sims_adu_voc_records
+      sims_chn_ivi_records_merged = sims_chn_ivi_records
+      sims_chn_voc_records_merged = sims_chn_voc_records
     }
   }
 }
+simTypes = simTypes2merge
+recordings = recordings2merge
 sims_df_merged <- sims_df_merged[, -which(names(sims_df_merged) == "simDist")]
-write.csv(sims_df_merged, file = "~/Documents/GitHub/vocal-response-analysis-simulation/sims_df_merged_20250521.csv")
+#write.csv(sims_df_merged, file = "~/Documents/GitHub/vocal-response-analysis-simulation/sims_df_merged_20250521.csv")
+rm(sims_adu_ivi_records,sims_adu_voc_records,sims_chn_ivi_records,sims_chn_voc_records,simTypes2merge,recordings2merge)
+save.image(file = "~/Documents/GitHub/vocal-response-analysis-simulation/mergedSimData.Rdat")
