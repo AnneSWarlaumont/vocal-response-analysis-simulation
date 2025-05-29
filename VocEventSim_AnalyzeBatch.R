5279b4c96982a0415d6ef72604ea04f42fb533a2
Anne S. Warlaumont <warlaumont@ucla.edu>
2025-05-29 15:40:43 -0700

    Update VocEventSim_AnalyzeBatch.R
    Worked on adding ivi response regression analyses.

--- VocEventSim_AnalyzeBatch.R
+++ VocEventSim_AnalyzeBatch.R
@@ -90,11 +90,19 @@ write.csv(bestFitDists_details, file = "data/bestFitDists_details.csv")
 write.csv(bestFitDists_stats, file = "data/bestFitDists_stats.csv")
 
 ################################################################################
-# To-do: for the best-matched simulations,
+# For the best-matched simulations,
 # analyze the chn and adu ivis to test for response effects on ivis using:
 # 1. no control for previous ivi,
 # 2. control for previous ivi, or
 # 3. control for 3 previous ivis
+#
+# At the moment, this code does a separate analysis for each recording
+# But it would probably be more ideal to do analysis across recordings,
+# with recording as a random effect.
+#
+# I would also like to write code to analyze the human recordings, although
+# to do much with that I should get the data together for a larger dataset (n>3)
+# and we should probably pre-register those analyses, for maximal impact.
 ################################################################################
 
 for (simTypeToA in simTypesToAnalyze){
@@ -103,6 +111,12 @@ for (simTypeToA in simTypesToAnalyze){
     # Load in the simulation data
     recording_file = paste("data/",recordingToA,"/",simTypeToA,"/VocEventSim_Optimize.RData",sep="")
     load(recording_file)
+    
+    if (simTypeToA == "nonInteractive"){
+      fitOrder = fitOrder_noTurns
+    } else{
+      fitOrder = fitOrder_wTurns
+    }
     
     simID = 0
     ivi_records = c()
@@ -114,62 +128,56 @@ for (simTypeToA in simTypesToAnalyze){
     for (fitRank in 1:20){
 
       simID = simID + 1
-      #ivi_series = sims_chn_ivi_records[[fitOrder[fitRank]]] # HERE in adaptation of old code below.
+      
+      ivi_series = sims_chn_ivi_records[[fitOrder[fitRank]]]
+      chn_voc_record = sims_chn_voc_records[[fitOrder[fitRank]]]
+      adu_voc_record = sims_adu_voc_records[[fitOrder[fitRank]]]
+      
+      n_ivi = length(ivi_series)
+      
+        ivi_r_record = c() # initialize the record of when adu responded to chn
+        t = 1
+        for (i in 1:n_ivi){
+          if (ivi_series[i]==1){
+            ivi_r_record[i] = NA
+          } else if (sum(adu_voc_record[(t+1):(t+rthresh)],na.rm = TRUE)>0){
+            ivi_r_record[i] = 1
+          } else{
+            ivi_r_record[i] = 0
+          }
+          t = t+ivi_series[i]
+        }
+
+        ivi_records = c(ivi_records,ivi_series[4:n_ivi])
+        ivi_r_records = c(ivi_r_records,ivi_r_record[4:n_ivi])
+        simIDs = c(simIDs,rep(simID,(n_ivi-3)))
+        
+        # Correlate current IVI with previous IVI and get the residuals of that correlation
+        previvi_model = lm(scale(log(ivi_series[2:n_ivi]))~scale(log(ivi_series[1:(n_ivi-1)])))
+        previvi_resid = resid(previvi_model)
+        previvi_resids = c(previvi_resids,previvi_resid[3:(n_ivi-1)])
+        
+        # Correlate current IVI with time since the past 3 a1 IVIs and get the residuals of that correlation
+        prev3ivi_model = lm(scale(log(ivi_series[4:n_ivi]))~scale(log(ivi_series[3:(n_ivi-1)]))+(scale(log(ivi_series[3:(n_ivi-1)])+log(ivi_series[2:(n_ivi-2)])))+(scale(log(ivi_series[3:(n_ivi-1)])+log(ivi_series[2:(n_ivi-2)])+log(ivi_series[1:(n_ivi-3)]))))
+        prev3ivi_resid = resid(prev3ivi_model)
+        prev3ivi_resids = c(prev3ivi_resids,prev3ivi_resid)
+      
     }
+    
+    # Analyze using IVI-based approaches (our original and its variants controlling for previous IVIs)
+    ivi_models = analyze_ivis(ivi_records,ivi_r_records,previvi_resids,prev3ivi_resids,simIDs)
+    uncontrolled_response_model = ivi_models[[1]]
+    residual_response_model = ivi_models[[2]]
+    prev3residual_response_model = ivi_models[[3]]
+
+    summary(uncontrolled_response_model)
+    summary(residual_response_model)
+    summary(prev3residual_response_model)
     
   }
 }
 
 #### Code below is not yet adapted. Will eventually be adapted or deleted.
-#
-# 
-# for (fitRank in 1:20){
-#   
-#   ivi_series = sims_chn_ivi_records[[fitOrder[fitRank]]]
-#   chn_voc_record = sims_chn_voc_records[[fitOrder[fitRank]]]
-#   adu_voc_record = sims_adu_voc_records[[fitOrder[fitRank]]]
-#   
-#   n_ivi = length(ivi_series)
-#   
-#   
-#   ivi_r_record = c() # initialize the record of when adu responded to chn
-#   t = 1
-#   for (i in 1:n_ivi){
-#     if (ivi_series[i]==1){
-#       ivi_r_record[i] = NA
-#     } else if (sum(adu_voc_record[(t+1):(t+rthresh)],na.rm = TRUE)>0){
-#       ivi_r_record[i] = 1
-#     } else{
-#       ivi_r_record[i] = 0
-#     }
-#     t = t+ivi_series[i]
-#   }
-#   
-#   ivi_records = c(ivi_records,ivi_series[4:n_ivi])
-#   ivi_r_records = c(ivi_r_records,ivi_r_record[4:n_ivi])
-#   simIDs = c(simIDs,rep(simID,(n_ivi-3)))
-#   
-#   # Correlate current IVI with previous IVI and get the residuals of that correlation
-#   previvi_model = lm(scale(log(ivi_series[2:n_ivi]))~scale(log(ivi_series[1:(n_ivi-1)])))
-#   previvi_resid = resid(previvi_model)
-#   previvi_resids = c(previvi_resids,previvi_resid[3:(n_ivi-1)])
-#   
-#   # Correlate current IVI with time since the past 3 a1 IVIs and get the residuals of that correlation
-#   prev3ivi_model = lm(scale(log(ivi_series[4:n_ivi]))~scale(log(ivi_series[3:(n_ivi-1)]))+(scale(log(ivi_series[3:(n_ivi-1)])+log(ivi_series[2:(n_ivi-2)])))+(scale(log(ivi_series[3:(n_ivi-1)])+log(ivi_series[2:(n_ivi-2)])+log(ivi_series[1:(n_ivi-3)]))))
-#   prev3ivi_resid = resid(prev3ivi_model)
-#   prev3ivi_resids = c(prev3ivi_resids,prev3ivi_resid)
-#   
-# }
-# 
-# # Analyze using IVI-based approaches (our original and its variants controlling for previous IVIs)
-# ivi_models = analyze_ivis(ivi_records,ivi_r_records,previvi_resids,prev3ivi_resids,simIDs)
-# uncontrolled_response_model = ivi_models[[1]]
-# residual_response_model = ivi_models[[2]]
-# prev3residual_response_model = ivi_models[[3]]
-# 
-# summary(uncontrolled_response_model)
-# summary(residual_response_model)
-# summary(prev3residual_response_model)
 # 
 # #################################################################################
 # # Plot fit to human data as a function of parameter value
